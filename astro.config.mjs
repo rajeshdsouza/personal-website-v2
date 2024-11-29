49573283a75ea57870704e2058554a2279ca1440
rajeshdsouza <rajesh@rajeshdsouza.com>
2024-11-29 12:55:51 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { astroContent } from '@astrojs/content';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), astroContent()],
+  content: {
+    // Define a new content collection for blogs
+    collections: {
+      blogs: {
+        type: 'content', // Indicates that it's a content collection
+        schema: {
+          title: 'string',
+          date: 'date',
+          description: 'string',
+          author: 'string',
+        },
+      },
+    },
+  },
 });
